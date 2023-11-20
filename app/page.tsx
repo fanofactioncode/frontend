5725204aee4a149e23f919d1592cba47cc638b0c
Chandan Jal <chandan.jal.code@gmail.com>
2023-11-20 23:42:41 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="container p-4">
+      <Button>Click</Button>
+    </main>
+  );
+}
