289a150981398c9b0acec07111dced4ff9ba9c85
Chandan Jal <chandan.jal.code@gmail.com>
2024-09-01 22:55:44 +0530

    feat: theme color changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,16 @@ import PageLayout from "@/components/layout/page-layout";
 export default function Home() {
   return (
     <PageLayout>
-      <main className="container">
-        <h1>Home</h1>
+      <main className="container space-y-4 py-16">
+        <h1 className="text-center text-4xl font-bold text-text">
+          Epic Cinematic Revival: The Return of a Blockbuster - Movie Rerelease
+          Spectacular!
+        </h1>
+        <p className="text-center text-sm text-text-sub">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          quam aliquam id nam? Incidunt quidem voluptatibus facilis soluta
+          itaque eveniet!
+        </p>
       </main>
     </PageLayout>
   );
