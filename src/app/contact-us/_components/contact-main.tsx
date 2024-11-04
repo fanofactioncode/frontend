c8b4579c2e9c50deb8fd1d01236e820d3271e07e
Chandan Jal <chandan.jal.code@gmail.com>
2024-11-04 23:53:08 +0530

    refactor(contact): social links updated

--- src/app/contact-us/_components/contact-main.tsx
+++ src/app/contact-us/_components/contact-main.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import Link from "next/link";
 
-import {
-  FacebookCircleIcon,
-  InstagramCircleIcon,
-  TwitterCircleIcon,
-} from "@/components/icons";
 import { Badge } from "@/components/ui/badge";
 
 import { ContactForm } from "./contact-form";
+import { SocialLinks } from "./social-links";
 
 export function ContactMain() {
   return (
@@ -22,32 +17,7 @@ export function ContactMain() {
             Get in touch with us. We&apos;re here to assist you.
           </h1>
 
-          <div className="bottom-0 right-0 flex flex-col gap-2 sm:absolute sm:bottom-24">
-            <Link
-              href="https://www.facebook.com/WeAreFanOfAction/"
-              aria-label="Facebook"
-              target="_blank"
-              tabIndex={-1}
-            >
-              <FacebookCircleIcon />
-            </Link>
-            <Link
-              href="https://www.instagram.com/fanofaction"
-              aria-label="Instagram"
-              target="_blank"
-              tabIndex={-1}
-            >
-              <InstagramCircleIcon />
-            </Link>
-            <Link
-              href="https://www.youtube.com/@WeAreFanOfAction"
-              aria-label="Youtube"
-              target="_blank"
-              tabIndex={-1}
-            >
-              <TwitterCircleIcon />
-            </Link>
-          </div>
+          <SocialLinks />
         </div>
       </div>
       <ContactForm />
---
+++ src/app/contact-us/_components/social-links.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+import {
+  FacebookCircleIcon,
+  InstagramCircleIcon,
+  TwitterCircleIcon,
+} from "@/components/icons";
+
+export function SocialLinks() {
+  return (
+    <div className="bottom-0 right-0 flex flex-col gap-2 sm:absolute sm:bottom-24">
+      <Link
+        href="https://www.facebook.com/WeAreFanOfAction/"
+        aria-label="Facebook"
+        target="_blank"
+        tabIndex={-1}
+      >
+        <FacebookCircleIcon />
+      </Link>
+      <Link
+        href="https://www.instagram.com/fanofaction"
+        aria-label="Instagram"
+        target="_blank"
+        tabIndex={-1}
+      >
+        <InstagramCircleIcon />
+      </Link>
+      <Link
+        href="https://www.youtube.com/@WeAreFanOfAction"
+        aria-label="Youtube"
+        target="_blank"
+        tabIndex={-1}
+      >
+        <TwitterCircleIcon />
+      </Link>
+    </div>
+  );
+}
