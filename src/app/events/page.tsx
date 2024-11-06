3ec62df9ed0fd9b9638fa3133c9fdb39aae42d6a
Chandan Jal <chandan.jal.code@gmail.com>
2024-11-06 22:08:37 +0530

    revert: dynamic event page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -3,8 +3,6 @@ import React from "react";
 import RunningShows from "./_components/running-shows";
 import UpcomingMovies from "./_components/upcoming-movies";
 
-export const dynamic = "force-dynamic";
-
 export default async function EventsPage() {
   return (
     <>
