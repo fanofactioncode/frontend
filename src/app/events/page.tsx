e33a4c527e7dc88bd9d078211d104ad870850c07
Chandan Jal <chandan.jal.code@gmail.com>
2024-11-06 22:05:19 +0530

    feat: made events page dynamic

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import RunningShows from "./_components/running-shows";
 import UpcomingMovies from "./_components/upcoming-movies";
 
+export const dynamic = "force-dynamic";
+
 export default async function EventsPage() {
   return (
     <>
