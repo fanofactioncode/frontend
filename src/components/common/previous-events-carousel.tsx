30f7aa6b947ac58de17df187b8fd43c93ba900ca
Chandan Jal <chandan.jal.code@gmail.com>
2024-12-31 23:48:23 +0530

    fix(content): spelling changed

--- src/components/common/previous-events-carousel.tsx
+++ src/components/common/previous-events-carousel.tsx
@@ -54,7 +54,7 @@ export default function PreviousEventsCarousel() {
                 </div>
                 <div className="mt-2 flex-1 space-y-1">
                   <span className="rounded-md bg-[#ABB7C4]/50 px-[8px] py-[5px] text-xs font-semibold text-text">
-                    18 Fab, 2024
+                    18 Feb, 2024
                   </span>
                   <h3 className="text-base font-semibold text-text">
                     Phir Hera Pheri
