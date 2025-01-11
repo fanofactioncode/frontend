e96799b9707f64995003980ea1bf178fc7ae009a
Chandan Jal <chandan.jal.code@gmail.com>
2025-01-11 23:33:22 +0530

    chore: metadata updated for events

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,3 +1,5 @@
+import { Metadata } from "next";
+
 import env from "@/lib/env";
 import { RecentlyShow } from "@/types/show.type";
 
@@ -5,6 +7,12 @@ import RunningShows from "./_components/running-shows";
 import UpcomingMovies from "./_components/upcoming-movies";
 
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "The Ultimate Destination for Movie Rereleases",
+  description:
+    "FanOfAction brings you the latest updates on movie rereleases, iconic films returning to theaters. Join us to relive cinematic magic like never before!",
+};
 
 async function getCurrentlyRunningShows(): Promise<RecentlyShow[]> {
   const res = await fetch(env.NEXT_PUBLIC_API_URL + "/shows/popular", {
