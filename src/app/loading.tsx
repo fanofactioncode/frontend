5fe901044de6537f55ac9b3695d5115bb8cce6d5
Chandan Jal <chandan.jal.code@gmail.com>
2025-01-21 22:18:26 +0530

    style(theme): loading colors updated

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -7,7 +7,7 @@ export default function Loading() {
       >
         <svg
           aria-hidden="true"
-          className="h-10 w-10 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+          className="h-10 w-10 animate-spin fill-primary text-primary-foreground"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
